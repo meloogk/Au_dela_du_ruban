8c233b9f7042447d50d51d27287c55b697344b73
meloogk <gouakoumelvinpro@gmail.com>
2026-01-29 21:27:32 +0000

    modification mineure

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Heart, Shield, Users, ArrowRight } from "lucide-react";
+import { Heart,  ArrowRight } from "lucide-react";
 
 const Hero = () => {
   return (
@@ -23,8 +23,8 @@ const Hero = () => {
           {/* Content */}
           <div className="max-w-2xl">
             <div className="animate-fade-up">
-              <span className="inline-flex items-center gap-2 rounded-full bg-primary/10 px-4 py-2 text-sm font-medium text-primary mb-6">
-                <Heart className="h-4 w-4" />
+              <span className="inline-flex items-center gap-2 rounded-full bg-pink-300 px-4 py-2 text-sm font-medium text-primary mb-6">
+                <Heart className="h-4 w-4 text-pink-600" />
                 Octobre Rose — Mois de sensibilisation
               </span>
             </div>
@@ -40,11 +40,11 @@ const Hero = () => {
             </p>
 
             <div className="animate-fade-up-delay-2 flex flex-col sm:flex-row gap-4 mb-12">
-              <Button variant="hero" size="xl">
+              <Button className="bg-pink-400 text-black">
                 Se faire dépister
                 <ArrowRight className="ml-2 h-5 w-5" />
               </Button>
-              <Button variant="heroOutline" size="xl">
+              <Button className="bg-black text-pink-400" >
                 En savoir plus
               </Button>
             </div>
@@ -71,12 +71,7 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Bottom wave */}
-      <div className="absolute bottom-0 left-0 right-0">
-        <svg viewBox="0 0 1440 120" className="w-full h-auto fill-background">
-          <path d="M0,64L48,69.3C96,75,192,85,288,80C384,75,480,53,576,48C672,43,768,53,864,64C960,75,1056,85,1152,80C1248,75,1344,53,1392,42.7L1440,32L1440,120L1392,120C1344,120,1248,120,1152,120C1056,120,960,120,864,120C768,120,672,120,576,120C480,120,384,120,288,120C192,120,96,120,48,120L0,120Z" />
-        </svg>
-      </div>
+      
     </section>
   );
 };
